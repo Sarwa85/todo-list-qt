e1cdf81829a3161298cac444eeab0e39d26c8d1f
Artur Sarwiński <artur.sarwinski@gmail.com>
2020-09-01 19:10:55 +0200

    Ikony w delegatorze

--- app/widgets/taskdelegate.cpp
+++ app/widgets/taskdelegate.cpp
@@ -17,18 +17,22 @@ void TaskDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option, 
     auto state = index.data(TaskModel::RoleState).value<TaskModel::TaskState>();
     painter->save();
     QBrush brush;
+    QIcon icon;
     painter->setPen(QPen(QColor("transparent")));
     switch (state) {
     case TaskModel::TaskWaitingAdd: {
         brush = QBrush(QColor("#73CB79"));
+        icon = QIcon::fromTheme("list-add");
         break;
     }
     case TaskModel::TaskWaitingRemove: {
         brush = QBrush(QColor("#CB7373"));
+        icon = QIcon::fromTheme("list-remove");
         break;
     }
     case TaskModel::TaskWaitingEdit: {
         brush = QBrush(QColor("#73B6CB"));
+        icon = QIcon::fromTheme("accessories-text-editor");
         break;
     }
     default: {
@@ -42,5 +46,13 @@ void TaskDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option, 
     if (option.state & QStyle::State_Selected)
           painter->fillRect(option.rect, option.palette.highlight());
 
-    painter->drawText(option.rect, m_text_flags, task.title);
+    int icon_h = option.rect.height() * 0.8;
+    if (!icon.isNull()) {
+        int y = option.rect.y() + (option.rect.height() - icon_h) / 2;
+        QRect rect(option.rect.x() + option.rect.width() - icon_h, y, icon_h, icon_h);
+        icon.paint(painter, rect);
+    }
+
+    int h_space = option.rect.width() - (icon.isNull() ? 0 : icon_h);
+    painter->drawText(QRect(option.rect.x(), option.rect.y(), h_space, option.rect.height()), m_text_flags, task.title);
 }
